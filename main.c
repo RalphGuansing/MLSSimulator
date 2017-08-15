73607674892bf7f488f63dc4a059c9b4b0c258d4
joecatarata <joecatarata@gmail.com>
2017-08-15 12:46:11 +0800

    Done with initial structs

--- main.c
+++ main.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#define TRUE 1
+#define FALSE (!(FALSE))
+#define userID_MIN 10000000
 
-struct subject{
+typedef int Boolean;
+typedef char String[101];
 
-};
+
+typedef struct{
+    int courseNumber;
+    int section;
+    String name;
+
+} subject;
+
+typedef struct{
+    int id;
+    String name;
+    String courseName;
+    Boolean isAdmin;
+}user;
 
 int main(){
     FILE *fp;
+    Boolean isLoggedIn;
+    int loggedInID;
 
 }
 
